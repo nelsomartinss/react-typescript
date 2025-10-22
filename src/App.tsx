0097c94359eb3ea25c5832d9a51061eef942541e
Nelson Martins <nelsonmartinsjp2001@gmail.com>
2025-10-22 12:15:58 -0300

    diferença de função, hook e componente

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,28 @@
+import { useState } from "react";
+
 export function App() {
-  return <></>;
+  return (
+    <>
+      <h1>Olá!</h1>
+    </>
+  );
 }
+
+// função js
+const soma = () => {
+  return 2 + 2;
+};
+soma();
+
+// react hook
+const useSoma = () => {
+  const [soma] = useState(2 + 2);
+  return soma;
+};
+useSoma();
+
+// componente react funcional
+export function ComponenteFuncional() {
+  return <h2>Componente Funcional</h2>;
+}
+// componentes nativos do react usam letra minuscula, mas nome de componentes personalizados usam letra maiuscula e retornam html (jsx)
