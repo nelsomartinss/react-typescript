70fab89be5a2722da553722d6c970d1f97ff34aa
Nelson Martins <nelsonmartinsjp2001@gmail.com>
2025-10-23 15:28:06 -0300

    entendendo eventos, keys, map, spread e useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,37 @@
 import { useState } from "react";
 
 export function App() {
-  const [hide, setHide] = useState(false); // o valor de hide é falso, então enquanto hide for falso a mensagem que aparece é "Não clicou!". Se clicarmos o hide muda para true e a mensagem passa a ser "Clicou!"
+  const [value, setValue] = useState("");
+  const [list, setList] = useState([
+    { id: "1", label: "fazer café" },
+    { id: "2", label: "estudar react" },
+    { id: "3", label: "ir ao mercado" },
+  ]);
+
   return (
-    <>
-      <button onClick={() => setHide(!hide)}>Clique aqui!</button>
-      {hide ? <p>Clicou!</p> : <p>Não clicou!</p>}
-    </>
+    <div>
+      <input
+        type="text"
+        value={value} // o valor é o mesmo do setValue
+        onChange={(e) => setValue(e.target.value)} // se alterar o valor, a variavel 'e' guarda esse valor e passar para o setValue, ele adiciona ao setValue o valor do input (target). Evento que adiciona do input ao value
+      />
+      <button
+        onClick={() => {
+          setList([
+            ...list, // o ... traz todo conteudo que ja existia na lista pra dentro do setList (novo array
+            { id: (list.length + 1).toString(), label: value }, // o proximo item é o tamanho da lista +1 transformado em string, o label é o valor que estava no input
+          ]);
+          setValue(""); // limpa o input apos adicionar
+        }}
+      >
+        Adicionar
+      </button>{" "}
+      {/* isso acontece quando clicamos nesse botão */}
+      <ol>
+        {list.map((listItem) => (
+          <li key={listItem.id}>{listItem.label}</li>
+        ))}
+      </ol>
+    </div>
   );
 }
