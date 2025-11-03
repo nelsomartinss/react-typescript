d6f4f702a744bd6c16cd4f0dc0fab15d0c7010b1
Nelson Martins <nelsonmartinsjp2001@gmail.com>
2025-11-03 18:40:06 -0300

    aprendendo sobre useRef

--- src/components/InputAdd/InputAdd.tsx
+++ src/components/InputAdd/InputAdd.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import InputAddStyles from './InputAdd.module.css';
 
 
@@ -6,20 +6,23 @@ interface IInputAddProps {
   onAdd(value: string): void;
 }
 export const InputAdd = (props: IInputAddProps) => {
+  const inputRef = useRef<HTMLInputElement>(null); // usamos o useRef para criar uma referência ao input
   const [value, setValue] = useState("");
 
   const handleAdd = () => {
     props.onAdd(value);
     setValue("");
+    inputRef.current?.focus(); // usando o focus para manter o cursor no input após adicionar, isso advém da referência do useRef
   };
   return (
     <div className={InputAddStyles.container}>
       <input
         className={InputAddStyles.input}
+        ref={inputRef} 
         type="text"
         value={value}
         onChange={(e) => setValue(e.target.value)}
-      />
+      /> {/* usamos o ref para pegar a referência do input */}
       <button className={InputAddStyles.button} onClick={handleAdd}>Adicionar</button>
     </div>
   );
