1af1a2a8e0d757f5811ad8072e12409d9a70f209
Nelson Martins <nelsonmartinsjp2001@gmail.com>
2025-11-03 18:51:16 -0300

    criando tela de login

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,32 @@ import { AppLayout } from "./shared/layout/AppLayout";
 import { About } from "./pages/About/About";
 import { NotFound } from "./pages/NotFound/NotFound";
 import { Details } from "./pages/Details/Details";
+import { Login } from "./pages/Login/Login";
 
 export const App = () => {
+  const isAuthenticated = false; // alterar para true para testar as rotas autenticadas
   return (
     <BrowserRouter>
-      <AppLayout>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/sobre" element={<About />} />
-          <Route path="/detalhes/:id" element={<Details />} /> {/* Rota com parametro id do useParams*/}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </AppLayout>
+      {/* se estiver autentivado */}
+      {isAuthenticated && (
+        <AppLayout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/sobre" element={<About />} />
+            <Route path="/detalhes/:id" element={<Details />} />{" "}
+            {/* Rota com parametro id do useParams*/}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </AppLayout>
+      )}
+      {/* se não estiver autentivado */}
+      {!isAuthenticated && (
+        <AppLayout>
+          <Routes>
+            <Route path="*" element={<Login />} />
+          </Routes>
+        </AppLayout>
+      )}
     </BrowserRouter>
   );
 };
